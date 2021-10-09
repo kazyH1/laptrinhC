bc7c8104727d381acad5cdcd2b7be2c6e02e5fe9
kazyH1 <90763632+kazyH1@users.noreply.github.com>
2021-10-09 22:29:52 +0700

    Add files via upload

--- ptbachai.cpp
+++ ptbachai.cpp
@@ -7,13 +7,15 @@ int main()
     do
     {
         int a,b,c;
-        float t;
+        float delta;
         cout << "phuong trinh co dang ax^2+bx+c=0 " << "\n";
         cout << "nhap a "; cin >> a;
         cout << "nhap b "; cin >> b;
         cout << "nhap c "; cin >> c;
-        if (a==0)
-        {
+        if (cin.fail()) { cout << "\n" << "LỖI. Bạn cần phải nhập a,b,c là số. " << endl ; return 0; }
+        else
+        { if (a==0)
+            {
             if(b==0)
             {
                 cout << "phuong trinh vo so nghiem"<< endl;
@@ -26,26 +28,28 @@ int main()
             {
                 cout << "phuong trinh co mot nghiem x = " << -c/b << endl;
             }
-        }
-        else
-        {
-            t=b*b-4*a*c;
-            if (t>0)
-            {
-                cout << "phuong trinh co nghiem x1 = " << (-b+ sqrt(t))/(a*2) << "\n";
-                cout << "phuong trinh co nghiem x2 = " << (-b- sqrt(t))/(a*2) << endl;
-            }
-            else if (t==0)
-            {
-                cout << "phuong trinh co nghiem kep x1 = x2 = " << (-b)/(2*a) << endl;
             }
             else
             {
-                cout << "phuong trinh vo nghiem" << endl;
-            }
-        }
+                delta=b*b-4*a*c;
+                if (delta>0)
+                {
+                    cout << "phuong trinh co nghiem x1 = " << (-b+ sqrt(delta))/(a*2) << "\n";
+                    cout << "phuong trinh co nghiem x2 = " << (-b- sqrt(delta))/(a*2) << endl;
+                }
+                else if (delta==0)
+                {
+                    cout << "phuong trinh co nghiem kep x1 = x2 = " << (-b)/(2*a) << endl;
+                }
+                else
+                {
+                    cout << "phuong trinh vo nghiem" << endl;
+                }
+        }}
+        
         cout << "do you want continue?(1-yes, 0-no): ";
         cin >>con;
     }
     while(con);
+    return 0;
 }
